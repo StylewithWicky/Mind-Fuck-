c8f48fa31e8932a7e5643b7a97e9f6721904df0f
Yazz <itsjustmaitai@gmail.com>
2025-08-07 09:27:17 +0300

    I did a router setup and installed the react-router-dom

--- mindstreak-frontend/src/main.jsx
+++ mindstreak-frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import LandingPage from "./components/LandingPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./Css/Landingpage.css"; // Global style
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
